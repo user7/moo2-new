7a452c0c60a3b2842402541b0cd1ee17718c9511
svr <svr@protonmail.com>
2024-01-20 00:31:08 +0300

    keyboard movement controls

--- rozha.gd
+++ rozha.gd
@@ -12,5 +12,11 @@ func _process(delta):
 		direction = -1
 	if Input.is_action_pressed("ui_right"):
 		direction = 1
+
 	rotation += angular_speed * direction * delta
-	position += Vector2.UP.rotated(rotation) * speed * delta
+
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
+
+	position += velocity * delta
