dfe6e6b26d3f695df721cf61731117c74bfd6276
svr <svr@protonmail.com>
2024-01-21 16:14:48 +0300

    shortcut, music

--- Main.gd
+++ Main.gd
@@ -15,6 +15,8 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	get_tree().call_group("mobs", "queue_free")
@@ -23,6 +25,7 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	$Music.play()
 
 func _on_score_timer_timeout():
 	score += 1
