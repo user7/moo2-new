719b3c2ae41af965128c4341dddb18de597f6f0e
svr <svr@protonmail.com>
2024-02-05 16:13:30 +0300

    cosmetics

--- starmap.gd
+++ starmap.gd
@@ -1,6 +1,10 @@
 extends Node
 
 @onready var camera = $SubViewportContainer/SubViewport/root/Camera2D
+var arrows = [
+	["move_left", 1, 0], ["move_right", -1, 0],
+	["move_down", 0, -1], ["move_up", 0, 1]
+]
 
 func _ready():
 	get_tree().root.connect("size_changed", _on_viewport_size_changed)
@@ -16,33 +20,30 @@ func _on_viewport_size_changed():
 	svc.position = Vector2.ZERO
 
 func _unhandled_input(event: InputEvent):
-	if event is InputEventMouseButton and event.pressed:
+	if event is InputEventMouseButton and event.pressed \
+		and event.button_index == MOUSE_BUTTON_LEFT:
 		var pos = event.position
 		var center = get_viewport().size / 2
 		var offset = Vector2(pos) - Vector2(center) 
 		camera.position += offset
-	
-func input_dir(input: String, val: int) -> int:
-	return val if Input.is_action_pressed(input) else 0
-
-func input_axis(name_pos: String, name_neg: String) -> int:
-	var v = input_dir(name_pos, 1)
-	return v if v else input_dir(name_neg, -1)
-
-func input_vec(a: String, b: String, c: String, d: String):
-	return Vector2(input_axis(a, b), input_axis(c, d))
 
 func input_zoom():
-	if Input.is_action_just_pressed("zoom in"):
-		return 1
-	return -1 if Input.is_action_just_pressed("zoom out") else 0
+	return 1 if Input.is_action_just_pressed("zoom in") else \
+		  -1 if Input.is_action_just_pressed("zoom out") else 0
+
+func input_sum(actions: Array):
+	var v = Vector2i.ZERO
+	for a in actions:
+		if Input.is_action_pressed(a[0]):
+			v += Vector2i(a[1], a[2])
+	return v
 
 func _process(_delta):
-	var shift = input_vec("move_left", "move_right", "move_up", "move_down")
+	var shift = input_sum(arrows)
 	var zoom = input_zoom()
-	if shift != Vector2.ZERO or zoom != 0:
+	if shift != Vector2i.ZERO or zoom != 0:
 		var cur_pos = camera.position
-		var new_pos = cur_pos - shift * 10
+		var new_pos = cur_pos - shift * 10.
 		var cur_zoom = camera.zoom
 		var new_zoom = cur_zoom + Vector2(zoom, zoom) * 0.1
 		for i in range(0, 2):
