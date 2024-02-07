5ffba4182874c615666a697b4ee2a80090cad0ae
svr <svr@protonmail.com>
2024-02-07 23:27:29 +0300

    simple game menu

--- s_main_screen.gd
+++ s_main_screen.gd
@@ -3,13 +3,13 @@ extends Node
 @onready var map = $MarkerMap
 
 var stars = [
-	[0, "Elena", 100, 100],
-	[1, "Kiji", 200, 200],
-	[2, "Marin", 300, 500],
-	[3, "Quaz", 100, 300],
-	[4, "Otter", 300, 100],
-	[5, "Katz", 200, 400],
-	[6, "Irma", 500, 500],
+	[0, "Blue", 100, 100],
+	[1, "White", 200, 200],
+	[2, "Yellow", 300, 500],
+	[3, "Orange", 100, 300],
+	[4, "Red", 300, 100],
+	[5, "Brown", 200, 400],
+	[6, "Black Hole", 500, 500],
 ]
 
 # Called when the node enters the scene tree for the first time.
@@ -19,8 +19,14 @@ func _ready():
 		scene.set_texture(load("res://img/star-%s.png" % s[0]))
 		scene.set_label(s[1])
 		map.add_marker(scene, Vector2(s[2], s[3]))
+	var menu = $GameMenu
+	menu.hide()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+func _on_game_pressed():
+	$GameMenu.show()
+
+func _on_return_pressed():
+	$GameMenu.hide()
